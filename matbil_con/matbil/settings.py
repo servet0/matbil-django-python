503a00e94b88a5bc1030fd49d9b446ee66ed7675
Servet Acar <servetacar540@gmail.com>
2023-12-02 15:18:41 +0300

    settings

--- matbil_con/matbil/settings.py
+++ matbil_con/matbil/settings.py
@@ -19,13 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!8k%$8fz1!^+4n&(k33=bge39y#dmwy#l5*^_b1p5%f)y%_8dv'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+# LOCAL_SETTİNGS.PY
 
 
 # Application definition
@@ -75,12 +69,15 @@ WSGI_APPLICATION = 'matbil.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# LOCAL_SETTİNGS.PY
+# settings.py dosyası
+
+# Diğer Django ayarları...
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
 
 
 # Password validation
